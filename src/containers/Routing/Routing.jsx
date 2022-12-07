c96fcfc9372495bca82ac4afef47f9a7d5a35cad
Gavin Stirling <gavinstir@gmail.com>
2022-12-07 13:24:48 +0000

    Setup routing

--- src/containers/Routing/Routing.jsx
+++ src/containers/Routing/Routing.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from '../Home/Home'
 
 const Routing = () => {
   return (
-    <div>Routing</div>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
   )
 }
 
