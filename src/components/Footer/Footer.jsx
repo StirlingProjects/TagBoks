52b0771bee37d773140d73b452c3e011dd9dae50
Gavin Stirling <gavinstir@gmail.com>
2022-12-07 15:50:44 +0000

    Incorporated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+
+import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <div className="footer">
+            <div>
+                <p className="footer__title">Please Note:</p>
+                <br />
+                <p>
+                    All information contained on this website is confidential
+                    and should never be shared with anyone outside of the
+                    TagBOKS squad. New players shall be provided access by the
+                    coaching and management team.
+                </p>
+                <br />
+                <p>
+                    Anybody found to be sharing this information with any
+                    outside party will be reported to the International Tag
+                    Federation, Tag SA and Try Tag Rugby and face a ban from all
+                    tag rugby events.
+                </p>
+            </div>
+            <br />
+            <div className="footer__links">
+                <div className="footer__useful">Useful</div>
+                <div className="footer__socials">Socials</div>
+            </div>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Home.scss";
+
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
@@ -11,27 +13,45 @@ const Home = () => {
         <div className="home">
             <Navbar />
             <Header title={"TagBoks - MXS"} background={saflag} />
-            <div className="body">
+            <div className="home__content">
                 <div className="home__left">
-                    The TagBOKS UK Squad is affiliated with Tag South Africa,
-                    the SA Rugby Union (SARU) and the International Tag
-                    Federation. The aim of the TagBOKS UK Squad is to represent
-                    Tag South Africa, SARU and, by extension, South Africa at
-                    the highest levels of Tag Rugby. We aim to provide an
-                    enjoyable environment for tag rugby players at all levels in
-                    the UK and Ireland, and a pathway to represent our country.
-                    We aim to have teams representing South Africa in all
-                    leagues across the UK where practicable, upholding the
-                    standards and behaviours that have been set. TagBOKS UK run
-                    regular training sessions and events that are open to all
-                    players with South African heritage and to promote the sport
-                    of Tag Rugby. We have some of the best coaches in the tag
-                    community, providing support and development for all
-                    players. Please explore the contents of the this webpage and
-                    feel free to contact the coaches and management team if you
-                    have any questions.
+                    <h2 className="home__title">
+                        Welcome to the South African TagBOKS Mixed Seniors Tag
+                        Rugby Webpage
+                    </h2>
+                    <br />
+                    <p>
+                        The TagBOKS UK Squad is affiliated with Tag South
+                        Africa, the SA Rugby Union (SARU) and the International
+                        Tag Federation. The aim of the TagBOKS UK Squad is to
+                        represent Tag South Africa, SARU and, by extension,
+                        South Africa at the highest levels of Tag Rugby.
+                    </p>
+                    <br />
+                    <p>
+                        We aim to provide an enjoyable environment for tag rugby
+                        players at all levels in the UK and Ireland, and a
+                        pathway to represent our country. We aim to have teams
+                        representing South Africa in all leagues across the UK
+                        where practicable, upholding the standards and
+                        behaviours that have been set.
+                    </p>
+                    <br />
+                    <p>
+                        TagBOKS UK run regular training sessions and events that
+                        are open to all players with South African heritage and
+                        to promote the sport of Tag Rugby. We have some of the
+                        best coaches in the tag community, providing support and
+                        development for all players.
+                    </p>
+                    <br />
+                    <p>
+                        Please explore the contents of the this webpage and feel
+                        free to contact the coaches and management team if you
+                        have any questions.
+                    </p>
                 </div>
-                <div className="home__right"></div>
+                <div className="home__right">RIGHT</div>
             </div>
             <Footer />
         </div>
