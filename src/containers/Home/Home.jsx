aade1cff10d1d17263a99f4d6fcdfa75b28f1f18
Gavin Stirling <gavinstir@gmail.com>
2022-12-07 15:31:11 +0000

    Started added home content

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
     return (
@@ -9,6 +10,29 @@ const Home = () => {
             <Navbar />
             <Header />
             TagBoks - MXS
+            <div className="body">
+                <div className="home__left">
+                    The TagBOKS UK Squad is affiliated with Tag South Africa,
+                    the SA Rugby Union (SARU) and the International Tag
+                    Federation. The aim of the TagBOKS UK Squad is to represent
+                    Tag South Africa, SARU and, by extension, South Africa at
+                    the highest levels of Tag Rugby. We aim to provide an
+                    enjoyable environment for tag rugby players at all levels in
+                    the UK and Ireland, and a pathway to represent our country.
+                    We aim to have teams representing South Africa in all
+                    leagues across the UK where practicable, upholding the
+                    standards and behaviours that have been set. TagBOKS UK run
+                    regular training sessions and events that are open to all
+                    players with South African heritage and to promote the sport
+                    of Tag Rugby. We have some of the best coaches in the tag
+                    community, providing support and development for all
+                    players. Please explore the contents of the this webpage and
+                    feel free to contact the coaches and management team if you
+                    have any questions.
+                </div>
+                <div className="home__right"></div>
+            </div>
+            <Footer />
         </div>
     );
 };
