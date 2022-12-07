c034b04a7c24fa35d8f8ba9b0d1c97a9349f1f6b
Gavin Stirling <gavinstir@gmail.com>
2022-12-07 14:17:11 +0000

    Setup links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+        <div className="navbar">
+            <h1 className="navbar__heading">Menu</h1>
+            <div className="navbar__links">
+                <NavLink to="/" className="navbar__link">Home</NavLink>
+                <NavLink to="/newplayer" className="navbar__link">New Player Zone</NavLink>
+                <NavLink to="/eligibility" className="navbar__link">Eligibility</NavLink>
+                <NavLink to="/squad" className="navbar__link">Squad List</NavLink>
+                <NavLink to="/playerhub" className="navbar__link">Player Hub</NavLink>
+                <NavLink to="/calendar" className="navbar__link">Training Calendar</NavLink>
+                <NavLink to="/gallery" className="navbar__link">Gallery</NavLink>
+                <NavLink to="/contacts" className="navbar__link">Contacts</NavLink>
+                <NavLink to="/archive" className="navbar__link">Archive</NavLink>
+            </div>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
