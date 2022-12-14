b1abf8e6d4b0fc63536056f6605940f14dc16b3c
Gavin Stirling <gavinstir@gmail.com>
2022-12-14 11:56:52 +0000

    Added contacts data

---
+++ src/assets/data/contact_info.js
@@ -0,0 +1,94 @@
+const ukMngmtArray = [
+    {
+        imageUrl: "",
+        name: "Jodie B",
+        role: "MXS Coach",
+        contact: "jmbijorac@outlook.com",
+    },
+    {
+        imageUrl: "",
+        name: "Emma B",
+        role: "MXS Coach",
+        contact: "e.brady56@gmail.com",
+    },
+    {
+        imageUrl: "",
+        name: "Piet D",
+        role: "MXS Manager",
+        contact: "piet.ddk@gmail.com",
+    },
+    {
+        imageUrl: "",
+        name: "Grant B",
+        role: "U.K. TagBOKS Coordinator",
+        contact: "grantbez@gmail.com",
+    },
+];
+
+const ukSupportArray = [
+    {
+        imageUrl: "",
+        name: "Natasha Glassford",
+        role: "Recruitment and Sponsorship",
+        contact: "",
+    },
+    {
+        imageUrl: "",
+        name: "Dean Berndt",
+        role: "Sponsorship",
+        contact: "",
+    },
+    {
+        imageUrl: "",
+        name: "Shannon Fayne-Hervey",
+        role: "Logistics",
+        contact: "",
+    },
+    {
+        imageUrl: "",
+        name: "Tahni-Anne Wilson",
+        role: "Social Media",
+        contact: "",
+    },
+    {
+        imageUrl: "",
+        name: "Kerry Briggs",
+        role: "Recruitment and Sponsorship",
+        contact: "",
+    },
+    {
+        imageUrl: "",
+        name: "Tinene Pieterse",
+        role: "Treasurer",
+        contact: "",
+    },
+    {
+        imageUrl: "",
+        name: "Miquela Bezuidenhoudt",
+        role: "Events and Recruitment",
+        contact: "",
+    },
+    {
+        imageUrl: "",
+        name: "Stephanie Lord",
+        role: "Events and Recruitment",
+        contact: "",
+    },
+];
+
+const wcMngmtArray = [
+    {
+        imageUrl: "",
+        name: "Stuart McConnell",
+        role: "President ITF",
+        contact: "stuart@taggroup.co.za",
+    },
+    {
+        imageUrl: "",
+        name: "Gilbert Modrau",
+        role: "Head of Coaching",
+        contact: "gilbertmodrau@gmail.com",
+    },
+];
+
+export default [ukMngmtArray, ukSupportArray, wcMngmtArray];
